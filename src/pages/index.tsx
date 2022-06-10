91d23d543b941c7d4776d04140890db18beb669b
hotter6163 <95465875+hotter6163@users.noreply.github.com>
2022-06-10 15:33:07 +0900

    Noto Sansフォントの導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,10 @@ import type { NextPage } from 'next'
 
 const Page: NextPage = () => {
   return (
-    <h1>Hello World!!</h1>
+    <>
+      <h1>Hello World!!</h1>
+      <h1>フォントの確認</h1>
+    </>
   )
 }
 
