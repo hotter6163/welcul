8429551e89dfc8749ad1cd0eb99fca75062df143
hotter6163 <95465875+hotter6163@users.noreply.github.com>
2022-06-10 16:04:09 +0900

    基本的なレイアウトの適用
    rayseeを見本にmax-witdhを設定
    muiのリセットcssを導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import CssBaseline from '@mui/material/CssBaseline'
+
 import "@fontsource/noto-sans-jp"
+import 'styles/globals.scss'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <CssBaseline />
       <Head>
         <title>新歓情報サイト</title>
       </Head>
-      <Component {...pageProps} />
+      <div className="display-field">
+        <Component {...pageProps} />
+      </div>
     </>
   )
 }
