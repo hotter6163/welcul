66972fdb0b6d68f941b5002d61b0a11ec9bf5f54
hotter6163 <95465875+hotter6163@users.noreply.github.com>
2022-06-12 16:02:26 +0900

    topページの背景色がpcだとうまくいかなかったバグを修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,39 +43,41 @@ const Page: NextPage<{ displayItems: DisplayItem[] }> = ({ displayItems }) => {
 
   return wrapInLayout('top',
     <Box component="main">
-      <Container maxWidth="xl" sx={{ bgcolor: 'background.main'}}>
-        <Grid container className="h-screen">
-          <Grid item xs={12} className="flex">
-            <div className="self-center w-full text-center">
-              <Typography
-                variant="h1"
-                sx={{ color: "text.accent" }}
-              >
-                新歓情報
-              </Typography>
-            </div>
+      <Box component="section" sx={{ bgcolor: 'background.main'}}>
+        <Container maxWidth="xl">
+          <Grid container className="h-screen">
+            <Grid item xs={12} className="flex">
+              <div className="self-center w-full text-center">
+                <Typography
+                  variant="h1"
+                  sx={{ color: "text.accent" }}
+                >
+                  新歓情報
+                </Typography>
+              </div>
+            </Grid>
+            <Grid item xs={12} className="flex">
+              <div className="self-center w-full">
+                <div className="text-center my-4">
+                  <Button variant="contained" color="accent" size="large">
+                    ユーザー登録して使い始める
+                  </Button>
+                </div>
+                <div className="text-center my-4">
+                  <Button variant="outlined" color="accent" size="large" className="bg-white">
+                    どんなイベントがあるか見る
+                  </Button>
+                </div>
+                <div className="text-center my-4">
+                  <Button variant="contained" color="secondary" size="large">
+                    ログイン
+                  </Button>
+                </div>
+              </div>
+            </Grid>
           </Grid>
-          <Grid item xs={12} className="flex">
-            <div className="self-center w-full">
-              <div className="text-center my-4">
-                <Button variant="contained" color="accent" size="large">
-                  ユーザー登録して使い始める
-                </Button>
-              </div>
-              <div className="text-center my-4">
-                <Button variant="outlined" color="accent" size="large" className="bg-white">
-                  どんなイベントがあるか見る
-                </Button>
-              </div>
-              <div className="text-center my-4">
-                <Button variant="contained" color="secondary" size="large">
-                  ログイン
-                </Button>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </Box>
       {renderedItems}
     </Box>
   )
